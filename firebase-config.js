3f4073b141689765d0752e06ef410d10b4965b3c
hax <56984134+habtamu-tamere@users.noreply.github.com>
2025-05-19 07:24:02 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,6 @@
 // Replace with your Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyCTqyN4RsgO8I2ejlz_rJn7DfqvBh0OhDI",
-  authDomain: "jobstation-b43d3.firebaseapp.com",
-  projectId: "jobstation-b43d3",
-  storageBucket: "jobstation-b43d3.firebasestorage.app",
-  messagingSenderId: "822682861338",
-  appId: "1:822682861338:web:400404b05918ebfc13f6a3",
-  measurementId: "G-JT88XXV9H4"
+  
 };
 
 // Initialize Firebase
